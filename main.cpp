f3572d394118969ec88e33ddca3245e5fa977b37
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-10-19 16:29:11 +0000

    INIT

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <boost/ut.hpp>
+#include <fmt/format.h>
+#include <numeric>
+#include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/take.hpp>
+#include <range/v3/view/transform.hpp>
+
+constexpr auto sum(auto... values) { return (values + ...); }
+
+int main() {
+  using namespace boost::ut;
+
+  "sum"_test = [] {
+    expect(sum(0) == 0_i);
+    expect(sum(1, 2) == 3_i);
+    expect(sum(1, 20) == 21_i);
+  };
+
+  "fmt"_test = [] {
+    expect(fmt::format("{}", 42) == "42");
+    expect(fmt::format("{} {}", "hello", "world") == "hello world");
+  };
+
+  "ranges"_test = [] {
+    using namespace ranges;
+    int sum = accumulate(views::ints(1, unreachable) |
+                             views::transform([](int i) { return i * i; }) |
+                             views::take(10),
+                         0);
+    expect(sum == 385);
+  };
+}
