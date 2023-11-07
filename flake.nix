4bf43e61f57a9766c8c2f91f94f05397bc0445a3
matthewcroughan <matt@croughan.sh>
2023-11-07 01:22:46 +0000

    initialise nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+      {
+        packages.default = pkgs.callPackage ./default.nix {};
+      };
+    };
+}
